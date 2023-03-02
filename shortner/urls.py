3378163432a81c323f0b73286373119f12a4b55c
MrSDT <mrdotsdts@gmail.com>
2023-03-02 05:08:50 +0330

    updated

--- shortner/urls.py
+++ shortner/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shorten/', views.shorten_url, name='shorten_url'),
     path('<str:short_code>/', views.redirect_original, name='redirect_original'),
     path('<str:short_code>/stats/', views.short_url_created, name='short_url_created'),
-    path('shorten/', views.generate_short_code, name='shorten_url'),
 ]
--- shortner/views.py
+++ shortner/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+import random
+import string
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from .forms import ShortURLForm
 from .models import ShortURL
 
@@ -21,30 +24,54 @@ def index(request):
     return render(request, 'index.html', {'form': form, 'title': title})
 
 
+def redirect_original(request, short_code):
+    """
+    Redirect to original URL based on short code
+    """
+    try:
+        short_url = ShortURL.objects.get(short_code=short_code)
+        short_url.num_clicks += 1
+        short_url.save()
+        return redirect(short_url.original_url)
+    except ShortURL.DoesNotExist:
+        return render(request, '404.html')
+
+
 def short_url_created(request, short_code):
     """
     Render page showing the original and shortened URLs, as well as the number of clicks
     """
     short_url = ShortURL.objects.get(short_code=short_code)
-    return render(request, 'shortened.html', {'short_url': short_url})
+    return render(request, 'stats.html', {'short_url': short_url})
+
+
+def generate_short_code():
+    """
+    Generate a unique short code for a URL
+    """
+    length = 6
+    while True:
+        short_code = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        try:
+            ShortURL.objects.get(short_code=short_code)
+        except ShortURL.DoesNotExist:
+            return short_code
 
 
 def shorten_url(request):
     """
-    Shorten URL and create new ShortURL object
+    Shorten a URL and save it to the database
     """
     if request.method == 'POST':
         form = ShortURLForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data['url']
-            short_url = ShortURL(original_url=url)
+            short_code = generate_short_code()
+            short_url = ShortURL(original_url=url, short_code=short_code)
             short_url.save()
-            short_code = str(short_url.id)
-            short_url.short_code = short_code
-            short_url.save()
-            return render(request, 'shortened.html', {'short_url': short_url})
+            short_url_url = reverse('short_url_created', args=[short_code])
+            return redirect(short_url_url)
     else:
         form = ShortURLForm()
 
-    return render(request, 'index.html', {'form': form})
-
+    return render(request, 'index.html', {'form': form, 'title': 'URL Shortener'})
