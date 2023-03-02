8f307c18a2580e2fa4c1da16ef2f40ee13d9dab4
MrSDT <mrdotsdts@gmail.com>
2023-03-02 05:23:29 +0330

    optimized

--- shortner/urls.py
+++ shortner/urls.py
@@ -5,6 +5,6 @@ app_name = 'shortener'
 urlpatterns = [
     path('', views.index, name='index'),
     path('shorten/', views.shorten_url, name='shorten_url'),
-    path('<str:short_code>/', views.redirect_original, name='redirect_original'),
-    path('<str:short_code>/stats/', views.short_url_created, name='short_url_created'),
+    path('<short_code>/', views.redirect_original, name='redirect_original'),
+    path('<short_code>/stats/', views.shorten_url_created, name='shorten_url_created'),
 ]
--- shortner/views.py
+++ shortner/views.py
@@ -1,9 +1,7 @@
-import random
-import string
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from .forms import ShortURLForm
 from .models import ShortURL
+from .forms import ShortURLForm
 
 
 def index(request):
@@ -17,7 +15,7 @@ def index(request):
             url = form.cleaned_data['url']
             short_url = ShortURL(original_url=url)
             short_url.save()
-            return render(request, 'shortened.html', {'short_url': short_url})
+            return redirect(reverse('shorten_url_created', args=[short_url.short_code]))
     else:
         form = ShortURLForm()
 
@@ -28,45 +26,31 @@ def redirect_original(request, short_code):
     """
     Redirect to original URL based on short code
     """
-    try:
-        short_url = ShortURL.objects.get(short_code=short_code)
-        short_url.num_clicks += 1
-        short_url.save()
-        return redirect(short_url.original_url)
-    except ShortURL.DoesNotExist:
-        return render(request, '404.html')
-
-
-def short_url_created(request, short_code):
-    """
-    Render page showing the original and shortened URLs, as well as the number of clicks
-    """
-    short_url = ShortURL.objects.get(short_code=short_code)
-    return render(request, 'stats.html', {'short_url': short_url})
-
-
-def generate_short_code():
-    """
-    Generate a unique short code for a URL
-    """
-    length = 6
-    while True:
-        short_code = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-        try:
-            ShortURL.objects.get(short_code=short_code)
-        except ShortURL.DoesNotExist:
-            return short_code
+    short_url = get_object_or_404(ShortURL, short_code=short_code)
+    short_url.num_clicks += 1
+    short_url.save()
+    return redirect(short_url.original_url)
 
 
 def shorten_url(request):
+    """
+    Render page with short URL created from original URL
+    """
+    short_url = None
     if request.method == 'POST':
         form = ShortURLForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data['url']
             short_url = ShortURL(original_url=url)
             short_url.save()
-            return render(request, 'shortened.html', {'short_url': short_url})
-    else:
-        form = ShortURLForm()
 
-    return render(request, 'index.html', {'form': form})
+    form = ShortURLForm()
+    return render(request, 'shorten.html', {'form': form, 'short_url': short_url})
+
+
+def shorten_url_created(request, short_code):
+    """
+    Render page showing the original and shortened URLs, as well as the number of clicks
+    """
+    short_url = get_object_or_404(ShortURL, short_code=short_code)
+    return render(request, 'stats.html', {'short_url': short_url})
